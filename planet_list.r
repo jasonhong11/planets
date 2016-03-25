a641f2d3fe08e369277121667ca6312dcd3db456
David <daan4786@ufl.edu>
2016-03-25 11:40:16 -0400

    add planet x

--- planet_list.r
+++ planet_list.r
@@ -2,3 +2,5 @@ planets = c('mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 
 print (planets)
 print ("But what about Pluto?")
 print ("Let's ignore Pluto for now.")
+We found a new planet, don't know the name yet.
+
